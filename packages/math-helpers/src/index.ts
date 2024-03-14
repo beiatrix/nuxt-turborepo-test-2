52058b5b3d67ab7ea687e77c8a8adfebd248df3e
beiatrix <bpedrasa@gmail.com>
2024-03-14 15:18:16 -0500

    feat(math-helpers): remove multiply

--- packages/math-helpers/src/index.ts
+++ packages/math-helpers/src/index.ts
@@ -5,7 +5,3 @@ export const add = (a: number, b: number) => {
 export const subtract = (a: number, b: number) => {
   return a - b;
 };
-
-export const multiply = (a: number, b: number) => {
-  return a * b;
-};
